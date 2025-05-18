77358893f74b8d3c7064633951633bc01382d717
Mo Abdo <mo.abdo_social@icloud.com>
2025-05-18 22:13:03 +0200

    funktioniert haa

--- src/main/java/Projekt1/LocalStorage.java
+++ src/main/java/Projekt1/LocalStorage.java
@@ -1,64 +1,94 @@
 package Projekt1;
 
-import akka.actor.typed.ActorRef;
-import akka.actor.typed.Behavior;
-import akka.actor.typed.javadsl.*;
+    import akka.actor.typed.ActorRef;
+    import akka.actor.typed.Behavior;
+    import akka.actor.typed.javadsl.*;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+    import java.time.Duration;
+    import java.util.HashMap;
+    import java.util.Map;
+    import java.util.Random;
 
-public class LocalStorage extends AbstractBehavior<LocalStorage.Command> {
+    public class LocalStorage extends AbstractBehavior<LocalStorage.Command> {
 
-    public interface Command {}
+        private boolean isOrdering = false;
+        private static final Duration ORDER_COOLDOWN = Duration.ofSeconds(30);
+        private int specialRequests = 0;
+        private static final int MINIMUM_SPECIAL_REQUESTS = 2;
 
-    public static class RequestSpecialRequests implements Command {
-        public final ActorRef<Worker.Command> worker;
-        public final ActorRef<ProductionLine.Command> productionLine;
+        public interface Command {}
 
-        public RequestSpecialRequests(ActorRef<Worker.Command> worker, ActorRef<ProductionLine.Command> productionLine) {
-            this.worker = worker;
-            this.productionLine = productionLine;
+        public static class CheckSpecialRequests implements Command {
+            public ActorRef<Worker.Command> worker = null;
+
+            public CheckSpecialRequests() {
+            }
+        }
+
+        private static class OrderComplete implements Command {}
+
+        public static class RequestSpecialRequests implements Command {
+            public final ActorRef<Worker.Command> worker;
+            public final ActorRef<ProductionLine.Command> productionLine;
+
+            public RequestSpecialRequests(ActorRef<Worker.Command> worker, ActorRef<ProductionLine.Command> productionLine) {
+                this.worker = worker;
+                this.productionLine = productionLine;
+            }
+        }
+
+        private final Map<String, Integer> storage = new HashMap<>();
+
+        public static Behavior<Command> create() {
+            return Behaviors.setup(LocalStorage::new);
+        }
+
+        private LocalStorage(ActorContext<Command> context) {
+            super(context);
+            storage.put("Ledersitze", 4);
+            storage.put("Klimaautomatik", 4);
+            storage.put("Elektrische Fensterheber", 4);
+            storage.put("Automatikgetriebe", 4);
+        }
+
+        @Override
+        public Receive<Command> createReceive() {
+            return newReceiveBuilder()
+                    .onMessage(RequestSpecialRequests.class, this::onRequestSpecialRequests)
+                    .onMessage(CheckSpecialRequests.class, this::onCheckSpecialRequests)
+                    .onMessage(OrderComplete.class, this::onOrderComplete)
+                    .build();
+        }
+
+        private Behavior<Command> onCheckSpecialRequests(CheckSpecialRequests msg) {
+            if (specialRequests < MINIMUM_SPECIAL_REQUESTS && !isOrdering) {
+                isOrdering = true;
+                getContext().getLog().info("Nicht genügend Spezialwünsche. Bestellung wird aufgegeben.");
+                getContext().scheduleOnce(ORDER_COOLDOWN, getContext().getSelf(), new OrderComplete());
+            }
+            return this;
+        }
+
+        private Behavior<Command> onOrderComplete(OrderComplete msg) {
+            isOrdering = false;
+            return this;
+        }
+
+        private Behavior<Command> onRequestSpecialRequests(RequestSpecialRequests msg) {
+            Random random = new Random();
+            String[] requests = storage.keySet().toArray(new String[0]);
+            String request1 = requests[random.nextInt(requests.length)];
+            String request2 = requests[random.nextInt(requests.length)];
+
+            if (storage.get(request1) > 0 && storage.get(request2) > 0) {
+                storage.put(request1, storage.get(request1) - 1);
+                storage.put(request2, storage.get(request2) - 1);
+                getContext().getLog().info("Spezialwünsche {} und {} entnommen.", request1, request2);
+            } else {
+                msg.worker.tell(new Worker.InstallSpecialRequests(getContext().getSelf(), msg.productionLine));
+                getContext().getLog().info("Nicht genügend Spezialwünsche. Bestellung wird aufgegeben.");
+                getContext().scheduleOnce(Duration.ofSeconds(random.nextInt(6) + 10), getContext().getSelf(), msg);
+            }
+            return this;
         }
     }
-
-    private final Map<String, Integer> storage = new HashMap<>();
-
-    public static Behavior<Command> create() {
-        return Behaviors.setup(LocalStorage::new);
-    }
-
-    private LocalStorage(ActorContext<Command> context) {
-        super(context);
-        storage.put("Ledersitze", 4);
-        storage.put("Klimaautomatik", 4);
-        storage.put("Elektrische Fensterheber", 4);
-        storage.put("Automatikgetriebe", 4);
-    }
-
-    @Override
-    public Receive<Command> createReceive() {
-        return newReceiveBuilder()
-                .onMessage(RequestSpecialRequests.class, this::onRequestSpecialRequests)
-                .build();
-    }
-
-    private Behavior<Command> onRequestSpecialRequests(RequestSpecialRequests msg) {
-        Random random = new Random();
-        String[] requests = storage.keySet().toArray(new String[0]);
-        String request1 = requests[random.nextInt(requests.length)];
-        String request2 = requests[random.nextInt(requests.length)];
-
-        if (storage.get(request1) > 0 && storage.get(request2) > 0) {
-            storage.put(request1, storage.get(request1) - 1);
-            storage.put(request2, storage.get(request2) - 1);
-            getContext().getLog().info("Spezialwünsche {} und {} entnommen.", request1, request2);
-        } else {
-            msg.worker.tell(new Worker.InstallSpecialRequests(getContext().getSelf(), msg.productionLine));
-            getContext().getLog().info("Nicht genügend Spezialwünsche. Bestellung wird aufgegeben.");
-            getContext().scheduleOnce(Duration.ofSeconds(new Random().nextInt(6) + 10), getContext().getSelf(), msg);
-        }
-        return this;
-    }
-}
--- src/main/java/Projekt1/Worker.java
+++ src/main/java/Projekt1/Worker.java
@@ -1,64 +1,78 @@
 package Projekt1;
 
-import akka.actor.typed.ActorRef;
-import akka.actor.typed.Behavior;
-import akka.actor.typed.javadsl.*;
+        import akka.actor.typed.ActorRef;
+        import akka.actor.typed.Behavior;
+        import akka.actor.typed.javadsl.*;
 
-import java.time.Duration;
-import java.util.Random;
+        import java.time.Duration;
+        import java.util.Random;
 
-public class Worker extends AbstractBehavior<Worker.Command> {
+        public class Worker extends AbstractBehavior<Worker.Command> {
 
-    public interface Command {}
+            public interface Command {}
 
-    public static class BuildCarBody implements Command {
-        public final int orderId;
-        public final ActorRef<ProductionLine.Command> productionLine;
+            private static class ResetStorageCheck implements Command {}
+            private boolean hasCheckedStorage = false;
 
-        public BuildCarBody(int orderId, ActorRef<ProductionLine.Command> productionLine) {
-            this.orderId = orderId;
-            this.productionLine = productionLine;
+            private Behavior<Command> onResetStorageCheck(ResetStorageCheck msg) {
+                hasCheckedStorage = false;
+                return this;
+            }
+
+            public static class BuildCarBody implements Command {
+                public final int orderId;
+                public final ActorRef<ProductionLine.Command> productionLine;
+
+                public BuildCarBody(int orderId, ActorRef<ProductionLine.Command> productionLine) {
+                    this.orderId = orderId;
+                    this.productionLine = productionLine;
+                }
+            }
+
+            public static class InstallSpecialRequests implements Command {
+                public final ActorRef<LocalStorage.Command> localStorage;
+                public final ActorRef<ProductionLine.Command> productionLine;
+
+                public InstallSpecialRequests(ActorRef<LocalStorage.Command> localStorage, ActorRef<ProductionLine.Command> productionLine) {
+                    this.localStorage = localStorage;
+                    this.productionLine = productionLine;
+                }
+            }
+
+            public static Behavior<Command> create(String name) {
+                return Behaviors.setup(context -> new Worker(context, name));
+            }
+
+            private final String name;
+
+            private Worker(ActorContext<Command> context, String name) {
+                super(context);
+                this.name = name;
+            }
+
+            @Override
+            public Receive<Command> createReceive() {
+                return newReceiveBuilder()
+                        .onMessage(BuildCarBody.class, this::onBuildCarBody)
+                        .onMessage(InstallSpecialRequests.class, this::onInstallSpecialRequests)
+                        .onMessage(ResetStorageCheck.class, this::onResetStorageCheck)
+                        .build();
+            }
+
+            private Behavior<Command> onBuildCarBody(BuildCarBody msg) {
+                getContext().getLog().info("{} baut die Karosserie für Auftrag {}", name, msg.orderId);
+                getContext().scheduleOnce(Duration.ofSeconds(new Random().nextInt(6) + 5), msg.productionLine, new ProductionLine.AssignWorker(getContext().getSelf()));
+                return this;
+            }
+
+            private Behavior<Command> onInstallSpecialRequests(InstallSpecialRequests msg) {
+                if (!hasCheckedStorage) {
+                    hasCheckedStorage = true;
+                    msg.localStorage.tell(new LocalStorage.CheckSpecialRequests());
+                    // Längere Wartezeit bis zur nächsten Überprüfung
+                    getContext().scheduleOnce(Duration.ofSeconds(10), getContext().getSelf(),
+                            new ResetStorageCheck());
+                }
+                return this;
+            }
         }
-    }
-
-    public static class InstallSpecialRequests implements Command {
-        public final ActorRef<Projekt1.LocalStorage.Command> localStorage;
-        public final ActorRef<ProductionLine.Command> productionLine;
-
-        public InstallSpecialRequests(ActorRef<LocalStorage.Command> localStorage, ActorRef<ProductionLine.Command> productionLine) {
-            this.localStorage = localStorage;
-            this.productionLine = productionLine;
-        }
-    }
-
-    public static Behavior<Command> create(String name) {
-        return Behaviors.setup(context -> new Worker(context, name));
-    }
-
-    private final String name;
-
-    private Worker(ActorContext<Command> context, String name) {
-        super(context);
-        this.name = name;
-    }
-
-    @Override
-    public Receive<Command> createReceive() {
-        return newReceiveBuilder()
-                .onMessage(BuildCarBody.class, this::onBuildCarBody)
-                .onMessage(InstallSpecialRequests.class, this::onInstallSpecialRequests)
-                .build();
-    }
-
-    private Behavior<Command> onBuildCarBody(BuildCarBody msg) {
-        getContext().getLog().info("{} baut die Karosserie für Auftrag {}", name, msg.orderId);
-        getContext().scheduleOnce(Duration.ofSeconds(new Random().nextInt(6) + 5), msg.productionLine, new ProductionLine.AssignWorker(getContext().getSelf()));
-        return this;
-    }
-
-    private Behavior<Command> onInstallSpecialRequests(InstallSpecialRequests msg) {
-        getContext().getLog().info("{} installiert Spezialwünsche.", name);
-        msg.localStorage.tell(new LocalStorage.RequestSpecialRequests(getContext().getSelf(), msg.productionLine));
-        return this;
-    }
-}
